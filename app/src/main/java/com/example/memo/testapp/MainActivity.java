f75db613fef35ae4ca24b1cca92c8577d1f2e24c
mohamed-ahmed101 <mohamed,ahmedelgawad@gmail.com>
2019-02-12 15:55:40 +0200

    u2

--- app/src/main/java/com/example/memo/testapp/MainActivity.java
+++ app/src/main/java/com/example/memo/testapp/MainActivity.java
@@ -10,8 +10,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void  add (Integer b )
+    public void  add (Integer xx )
     {
-        b++;
+        xx++;
+
+        xx--;
     }
 }
